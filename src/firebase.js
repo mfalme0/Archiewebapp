78231e40ae2593df2e6e3fe48fc95fe36e780610
Mfalme <76815758+mfalme0@users.noreply.github.com>
2024-11-09 16:33:42 +0300

    firebase hide
    since the repo is going public i am keeping my api keys to heart

--- src/firebase.js
+++ src/firebase.js
@@ -10,15 +10,7 @@ const {
 } = require("firebase/firestore");
 const express = require('express');
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB-aTs_Qt0DO8BgC8q0U2XC7tRRUGonQ-I",
-  authDomain: "archie-889f0.firebaseapp.com",
-  projectId: "archie-889f0",
-  storageBucket: "archie-889f0.appspot.com",
-  messagingSenderId: "527592657462",
-  appId: "1:527592657462:web:3a35ee207fc891efb17e97",
-  measurementId: "G-VRVHNCXXZ1"
-};
+import firebaseConfig from "./firebaseconfig"
 
 const app = express();
 const port = 8060;
---
+++ src/firebaseconfig.js
@@ -0,0 +1,10 @@
+
+ firebaseConfig = {
+    apiKey: "AIzaSyB-aTs_Qt0DO8BgC8q0U2XC7tRRUGonQ-I",
+    authDomain: "archie-889f0.firebaseapp.com",
+    projectId: "archie-889f0",
+    storageBucket: "archie-889f0.appspot.com",
+    messagingSenderId: "527592657462",
+    appId: "1:527592657462:web:3a35ee207fc891efb17e97",
+    measurementId: "G-VRVHNCXXZ1"
+  };
